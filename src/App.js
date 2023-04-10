7f86dc4f8683e4819ac385748974cbfc2235090d
Gun Lee <gun@Gunui-MacBookAir.local>
2023-04-10 18:40:31 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Calendar from './Screen/Calendar';
+
+function App() {
+  return (
+    <div className="App">
+      <Calendar />
+    </div>
+  );
+}
+
+export default App;
